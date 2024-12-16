b59517c1dd8c3e97c96f14c41bb1931bac0491f4
HGRgamer <72596840+HGRgamer@users.noreply.github.com>
2024-12-16 18:09:13 +0000

    Implemented user register (Task2)

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dbURL from "./src/config/db.config.js";
+
+import userAuthRoutes from "./src/routes/v1/userAuth.routes.js";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/v1/auth/", userAuthRoutes);
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}! 🚀`);
 });
+
 mongoose.connect(dbURL)
 .then(() => {
     console.log("Successfully connected to MongoDB Atlas");
