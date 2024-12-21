d2ec5e91fda8b9d6023f06bbac19489436143997
Darkknight0125 <samansidhu0125@gmail.com>
2024-12-21 14:04:07 +0530

    Update/ Remove List feature added

--- src/routes/v1/userLists.routes.js
+++ src/routes/v1/userLists.routes.js
@@ -2,9 +2,28 @@ import express from 'express';
 const router = express.Router();
 import userAuthMiddleware from '../../middlewares/userAuth.middleware.js';
 import { addAnimeToList, addGameToList, addMovieToList } from '../../controllers/userLists.controller.js';
+import {
+    updateAnimeInList,
+    updateGameInList,
+    updateMovieInList,
+    removeAnimeFromList,
+    removeGameFromList,
+    removeMovieFromList,
+  } from '../../controllers/userLists.controller.js';
 
 router.post('/anime/add', userAuthMiddleware, addAnimeToList);
 router.post('/games/add', userAuthMiddleware, addGameToList);
 router.post('/movies/add',userAuthMiddleware, addMovieToList);
+
+
+// Update routes
+router.patch('/anime/update/:id', userAuthMiddleware, updateAnimeInList);
+router.patch('/games/update/:id', userAuthMiddleware, updateGameInList);
+router.patch('/movies/update/:id', userAuthMiddleware, updateMovieInList);
+
+// Remove routes
+router.delete('/anime/remove/:id', userAuthMiddleware, removeAnimeFromList);
+router.delete('/games/remove/:id', userAuthMiddleware, removeGameFromList);
+router.delete('/movies/remove/:id', userAuthMiddleware, removeMovieFromList);
 
 export default router;
